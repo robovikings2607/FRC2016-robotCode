4da5a5a102a4da3ef9b7f54cab0411d3cbf3538d
David <davidr1324@gmail.com>
2016-02-06 16:57:44 -0500

    Initial Commit.

---
+++ PracticeBot 2016/src/org/usfirst/frc/team2607/robot/Constants.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc.team2607.robot;
+
+public class Constants {
+
+	//TODO Put all constants/port #'s in this class
+}
---
+++ PracticeBot 2016/src/org/usfirst/frc/team2607/robot/Transmission.java
@@ -0,0 +1,91 @@
+package org.usfirst.frc.team2607.robot;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+
+public class Transmission implements SpeedController {
+	
+	Talon motor1 , motor2 , motor3;
+	Solenoid shifter;
+	
+	SmoothedEncoder enc;
+	PIDController pidLoop;
+	
+	private boolean encodersFlag = false;
+	private boolean invertedFlag = false;
+	
+	/**
+	 * New SpeedController that models three motors turning the same axle. The first motor will run opposite to
+	 * the other motors, but at the same speed.
+	 * @param deviceID - PWM port of the first Talon. The other Talons are set to the next two ports.
+	 * @param useEncoders - whether you want to use encoders or not.
+	 */
+	public Transmission( int deviceID , boolean useEncoders ) {
+		
+		encodersFlag = useEncoders;
+		invertedFlag = false;
+		
+		motor1 = new Talon(deviceID);
+		motor2 = new Talon(deviceID + 1);
+		motor3 = new Talon(deviceID + 2);
+		
+		enc = new SmoothedEncoder(0 , 1 , true , Encoder.EncodingType.k1X);
+		shifter = new Solenoid(0);
+		
+		shifter.set(false);
+	}
+
+	@Override
+	public double get() {
+		if(!encodersFlag){
+			return motor1.get();
+		} else {
+			return -0.0;
+		}
+	}
+
+	@Override
+	public void set(double speed, byte syncGroup) {
+		set(speed);
+	}
+
+	@Override
+	public void set(double s) {
+		if(invertedFlag) {
+			s = -s;
+		}
+		if(!encodersFlag) {
+			motor1.set(-s);
+			motor2.set(s);
+			motor3.set(s);
+		}
+		else {
+			
+		}
+	}
+
+	@Override
+	public void setInverted(boolean isInverted) {
+		invertedFlag = isInverted;
+	}
+
+	@Override
+	public boolean getInverted() {
+		return invertedFlag;
+	}
+
+	@Override
+	public void disable() {
+		if (!encodersFlag) return;
+        enc.reset();
+	}
+	
+	@Override
+	public void pidWrite(double output) {
+		
+	}
+
+}
