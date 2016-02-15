326ce9c4dbb16c250435179f05120c7e660ee807
FRC2607Dev <FRC2607Dev@gmail.com>
2016-02-14 21:01:27 -0500

    updated CAN id's for comp bot drive train

--- CompBot 2016/src/org/usfirst/frc/team2607/robot/Constants.java
+++ CompBot 2016/src/org/usfirst/frc/team2607/robot/Constants.java
@@ -3,15 +3,15 @@ package org.usfirst.frc.team2607.robot;
 public class Constants {
 	
 	private static int 
-				leftMotor1 = 0	,
-				leftMotor2 = 1	,
-				leftMotor3 = 2	,
+				leftMotor1 = 1	,
+				leftMotor2 = 2	,
+				leftMotor3 = 3	,
 				
 				leftEncChannelA = 0	,
 				leftEncChannelB = 1	;
 	
 	private static int
-				rightMotor1 = 3	,
+				rightMotor1 = 6	,
 				rightMotor2 = 4	,
 				rightMotor3 = 5	,
 	
@@ -19,9 +19,9 @@ public class Constants {
 				rightEncChannelB = 3 ;
 	
 	public static int
-				armMotor = 6	,
-				puncherMotor = 7,
-				rollersMotor = 8,
+				armMotor = 7	,
+				puncherMotor = 8,
+				rollersMotor = 9,
 				
 				puncherLock = 0	,
 				clawOpener = 1	;
